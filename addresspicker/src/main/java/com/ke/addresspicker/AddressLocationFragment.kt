4cf588e55a4544506f5034039df7840a2faaf592
余艳辉 <913918146@qq.com>
2021-07-20 15:23:57 +0800

    1，移除不必要的权限。

--- addresspicker/src/main/java/com/ke/addresspicker/AddressLocationFragment.kt
+++ addresspicker/src/main/java/com/ke/addresspicker/AddressLocationFragment.kt
@@ -15,8 +15,7 @@ import com.amap.api.services.help.Tip
 import kotlinx.android.synthetic.main.fragment_address_location.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARGUMENT_TIP = "ARGUMENT_TIP"
 
 /**
--- addresspicker/src/main/java/com/ke/addresspicker/DelegateFragment.kt
+++ addresspicker/src/main/java/com/ke/addresspicker/DelegateFragment.kt
@@ -27,9 +27,8 @@ class DelegateFragment : Fragment() {
             .request(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.READ_PHONE_STATE
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                Manifest.permission.READ_PHONE_STATE
             ).subscribe {
                 if (it) {
                     startActivityForResult(Intent(requireContext(), HostActivity::class.java), 101)
