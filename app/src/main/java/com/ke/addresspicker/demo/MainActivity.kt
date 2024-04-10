11cb870bb6476e3e7428f17527d2dc393d032a96
keluokeda <913918146@qq.com>
2024-04-10 09:31:44 +0800

    1，升级高德导航到最新版本。

--- app/src/main/java/com/ke/addresspicker/demo/MainActivity.kt
+++ app/src/main/java/com/ke/addresspicker/demo/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
         MapsInitializer.updatePrivacyShow(application, true, true)
         MapsInitializer.updatePrivacyAgree(application, true)
 
